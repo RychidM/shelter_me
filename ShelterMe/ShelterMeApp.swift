2bc5dc00a4393a61a507ec24c5fecd47dce929da
RychidM <richardsk2@gmail.com>
2024-08-17 16:26:49 +0000

    Initial Commit

---
+++ ShelterMe/ShelterMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShelterMeApp.swift
+//  ShelterMe
+//
+//  Created by Rychid on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ShelterMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
